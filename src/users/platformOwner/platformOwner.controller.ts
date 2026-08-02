de4747057bd562f01e1b7d24f232165392fe4c36
AnouarMellouk-Engineer <anouarmellouk.info@gmail.com>
2026-08-02 12:23:25 +0100

    add platform owner controller

---
+++ src/users/platformOwner/platformOwner.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Put,
+} from '@nestjs/common';
+import { PlatformOwnerService } from './platformOwner.service';
+import { ZodValidationPipe } from 'src/zodValidationPipe';
+import updatePlatformOwnerSchema from './dto/updatePlatformOwner.dto';
+import type { UpdatePlatformOwnerDto } from './dto/updatePlatformOwner.dto';
+
+@Controller('users/platformOwners')
+export class PlatformOwnerController {
+  constructor(private platformOwnerService: PlatformOwnerService) {}
+
+  @Get(':id')
+  async getById(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.platformOwnerService.getById(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body(new ZodValidationPipe(updatePlatformOwnerSchema))
+    updatePlatformOwnerDto: UpdatePlatformOwnerDto,
+  ) {
+    return await this.platformOwnerService.update(id, updatePlatformOwnerDto);
+  }
+}
