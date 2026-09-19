3def5958e7f050c9854e4703e0cc45525d7e7a31
AnouarMellouk-Engineer <anouarmellouk.info@gmail.com>
2026-09-19 14:21:32 +0100

    refactor(user):protect school owner routes

--- src/modules/user/school-owner/school-owner.controller.ts
+++ src/modules/user/school-owner/school-owner.controller.ts
@@ -6,6 +6,7 @@ import {
   ParseUUIDPipe,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { SchoolOwnerService } from './school-owner.service';
 import {
@@ -13,29 +14,42 @@ import {
   type CreateSchoolOwnerDto,
 } from './dto/school-owner.dto';
 import { ZodValidationPipe } from 'src/common/pipes/zodValidation.pipe';
+import { Role } from 'src/database/enums';
+import { Roles } from 'src/modules/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/modules/auth/guards/roles.guard';
 
 @Controller('users/school-owners')
 export class SchoolOwnerController {
   public constructor(private readonly schoolOwnerService: SchoolOwnerService) {}
 
   @Get()
+  @Roles([Role.PLATFORM_OWNER])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   findAll() {
     return this.schoolOwnerService.findAll();
   }
 
   @Post()
+  @Roles([Role.PLATFORM_OWNER])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   create(
     @Body(new ZodValidationPipe(CreateSchoolOwnerSchema))
     dto: CreateSchoolOwnerDto,
   ) {
     return this.schoolOwnerService.create(dto);
   }
+
   @Get('filter')
+  @Roles([Role.PLATFORM_OWNER])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   filterByEmailOrUsername(@Query('search') search: string) {
     return this.schoolOwnerService.filter(search);
   }
 
   @Get(':id')
+  @Roles([Role.PLATFORM_OWNER])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   findBy(@Param('id', ParseUUIDPipe) id: string) {
     return this.schoolOwnerService.findOne(id);
   }
--- src/modules/user/school-owner/school-owner.module.ts
+++ src/modules/user/school-owner/school-owner.module.ts
@@ -3,7 +3,6 @@ import { SchoolOwnerController } from './school-owner.controller';
 import { SchoolOwnerService } from './school-owner.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/database/user.entity';
-import { SchoolModule } from 'src/modules/school/school.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
